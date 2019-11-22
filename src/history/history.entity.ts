00219c1b0998417077df6aef574f1bd875526198
hyc <992273994@qq.com>
2019-11-22 16:11:53 +0800

    '16:00'

--- src/history/history.entity.ts
+++ src/history/history.entity.ts
@@ -5,10 +5,9 @@ import {
   Entity, JoinColumn, JoinTable,
   ManyToMany, ManyToOne, OneToMany, OneToOne,
   PrimaryGeneratedColumn,
-  UpdateDateColumn,
+
 } from 'typeorm';
-import { FriendRequest } from '../friend-request/friendRequest.entity';
-import { Request } from '../request/request.entity';
+
 import { User } from '../user/user.entity';
 
 
--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { HistoryController } from './history.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { HistoryRepository } from './history.repository';
-import { UserRepository } from '../user/user.repository';
+
 import { NotifyRepository } from '../notify/notify.repository';
 
 @Module({
